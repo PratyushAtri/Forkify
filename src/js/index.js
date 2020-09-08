965b1dfb14e843bac79453f811bbf39bc6c64ce4
PratyushAtri <atripratyush16@gmail.com>
2020-09-08 21:13:25 +0530

    Searc

---
+++ src/js/index.js
@@ -0,0 +1,48 @@
+import Search from "./models/Search";
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from "./views/base";
+
+/* Global state of the app
+* - Search object
+* - Current Recipe object
+* - Shopping list object
+* - Liked recipes
+* */
+
+const state = {};
+
+const controlSearch = async () => {
+    // Get query from view
+    const query = searchView.getInput();
+
+    if (query) {
+        // New search object and add to state
+        state.search = new Search(query);
+
+        // Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        // Search for recipes
+        await state.search.getResults();
+
+        // Render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
